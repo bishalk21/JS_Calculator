0ab1d2cb9176405abca7e23148b331b79ea0057c
Your Name <karkibishal00@gmail.com>
2022-12-19 21:40:45 +1100

    javascript revised

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-const buttons = document.querySelectorAll(".btn-area>div");
-console.log(buttons); // returns a node list of all the buttons
-const display = document.querySelector(".display");
-console.log(display);
+const buttons = document.querySelectorAll(".btns-area>div");
+// console.log(buttons);
 
-const btnArr = Array.from(buttons); //convert node list to array
-console.log(btnArr);
+// to convert buttons node list to array we use Array.from() or spread operator or Array.prototype.slice.call()
+const buttonsArray = Array.from(buttons);
+// console.log(buttonsArray);
 
+const displaySec = document.querySelector(".display");
 let displayValue = "";
+
 let operators = ["+", "-", "*", "/"];
 let lastOperator = "";
 
-const displayElm = (str) => {
-  // displayElm is a function that takes a string and displays it on the screen
-  display.innerText = str || "0.00";
-};
-
-btnArr.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    const value = btn.innerText;
+// forEach method is used to iterate over an array and call a function for each element in the array
+buttonsArray.forEach((button) => {
+  button.addEventListener("click", function () {
+    const value = button.innerText;
     // console.log(value);
-    displayValue.innerText = value;
+    displaySec.innerText = value;
 
     if (value === "AC") {
       displayValue = "";
-      displayElm(displayValue);
+      display(displayValue);
       return;
     }
 
     if (value === "C") {
+      // slice method is used to extract a part of a string and return the extracted part in a new strin4
       displayValue = displayValue.slice(0, -1);
-      displayElm(displayValue);
+      display(displayValue);
       return;
     }
 
     if (operators.includes(value)) {
       lastOperator = value;
       // console.log(lastOperator);
+
       let lastChar = displayValue[displayValue.length - 1];
       // console.log(lastChar);
       if (operators.includes(lastChar)) {
         displayValue = displayValue.slice(0, -1);
-        displayElm(displayValue);
+        displayValue += value;
+        display(displayValue);
         return;
       }
     }
@@ -48,16 +48,12 @@ btnArr.forEach((btn) => {
     if (value === ".") {
       if (lastOperator) {
         const operatorIndex = displayValue.lastIndexOf(lastOperator);
+        // console.log(operatorIndex); //098+. --> 3
         const lastNumberSet = displayValue.slice(operatorIndex + 1);
-        if (lastNumberSet.includes(".")) {
-          console.log("already has a decimal");
-          return;
-        }
+        // console.log(lastNumberSet); //4 --> .
+        if (lastNumberSet.includes(".")) return;
       }
-      if (!lastOperator && displayValue.includes(".")) {
-        console.log("already has a decimal");
-        return;
-      }
+      if (!lastOperator && displayValue.includes(".")) return;
     }
 
     if (value === "=") {
@@ -69,20 +65,16 @@ btnArr.forEach((btn) => {
     }
 
     displayValue += value;
-    console.log(displayValue);
-    displayElm(displayValue);
+    display(displayValue);
   });
 });
-const total = () => {
-  const prankValue = randomNumber();
-  let audio = new Audio(`./Assets/a.wav`);
-  prankValue > 0 && audio.play() && (displayValue += prankValue);
-  const ttl = eval(displayValue) + prankValue;
-  displayValue = ttl.toString();
-  displayElm(ttl);
+
+const display = (str) => {
+  displaySec.innerText = str || "0.00";
 };
 
-const randomNumber = () => {
-  const num = Math.ceil(Math.random() * 10);
-  return num > 3 ? 0 : num;
+const total = () => {
+  const ttl = eval(displayValue);
+  displayValue = ttl.toFixed(2).toString();
+  display(displayValue);
 };
